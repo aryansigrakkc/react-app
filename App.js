9a2e809a815ec32245be64d0fd5401d4a2785ae6
aryansigrakkc <141600997+aryansigrakkc@users.noreply.github.com>
2023-08-15 10:55:46 +0530

    new repo created

---
+++ App.js
@@ -0,0 +1,9 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const heading=<>
+<h1>hello everyone this is react code</h1>
+</>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading)
